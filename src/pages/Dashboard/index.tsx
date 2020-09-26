66f667cc7a02f0f563b41118fa3690906127e1aa
henzovz <murilohezo@gmail.com>
2020-09-26 11:25:22 -0300

    estilização do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no GitHub</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no GitHub</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/28688721?s=460&u=9ba9e7217b90411786b435e86cead7b79df013d2&v=4"
+          alt="Murilo Henzo"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>bla bla bla bla</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/28688721?s=460&u=9ba9e7217b90411786b435e86cead7b79df013d2&v=4"
+          alt="Murilo Henzo"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>bla bla bla bla</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/28688721?s=460&u=9ba9e7217b90411786b435e86cead7b79df013d2&v=4"
+          alt="Murilo Henzo"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>bla bla bla bla</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,11 +1,102 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
+  font-size: 48px;
+  color: #3A3A3A;
+  max-width: 450px;
+  line-height: 56px;
 
-font-size: 48px;
-color: #3A3A3A;
+  margin-top: 80px;
 `;
 
-export const Container = styled.div`
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
 
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: "#A3A3A3";
+
+    &::placeholder {
+      color: "#A8A8B3";
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04D361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #FFF;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04D361')}
+    }
+  }
+
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #ffffff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    /* como é uma lista de ancoras, pode ser feita essa aplicação de css, uma elemento 'a' sendo prescedido de n 'a' */
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+    transform: translateX(10px);
+    }
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d3d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8d3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+
+
+
+  }
 `;
