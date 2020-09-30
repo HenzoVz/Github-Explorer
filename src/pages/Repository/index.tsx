679b90513fb7513332bb84d53bae622d4a2bab16
henzovz <murilohezo@gmail.com>
2020-09-30 15:04:02 -0300

    estlizando repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={20} />
+          Voltar
+        </Link>
+
+      </Header>
+
+      <RepositoryInfo>
+          <header>
+            <img />
+            <div>
+              <strong>title repository</strong>
+              <p>description</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>1080</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>49</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>78</strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to= "oaidjaod">
+          <div>
+            <strong>issue.title</strong>
+            <p>issue.user.login</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Repository;
